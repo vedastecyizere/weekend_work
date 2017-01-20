1288bf4ee8eb9639eabc64a0047101458b9bbe43
Vedaste Cyizere <revedaste@gmail.com>
2017-01-20 19:13:09 +0200

    Dealing with strings

--- algo/longest_word.rb
+++ algo/longest_word.rb
@@ -1,5 +1,3 @@
-# Complete the method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence
-
 def find_longest_word(sentence)
   words = sentence.split(" ")
 
